2382278e09814c512cbdc8184d692168a37484bd
Vupsen <k1x2310@gmail.com>
2015-05-09 00:02:36 +0300

    Update NaturalNumber.cpp

--- DMColloquium/NaturalNumber.cpp
+++ DMColloquium/NaturalNumber.cpp
@@ -11,6 +11,15 @@
 // put definitions for all methods over here.
 
 /// This is just an example of how to get going. Doesn't make any sense
+bool NaturalNumber::NZER_N_B()
+{
+	for (int i = 0; i < digitBlocks.size(); i++)
+	{
+		if (digitBlocks[i] != 0)
+			return true;
+	}
+		return false;
+}
 NaturalNumber NaturalNumber::greatestCommonFactor(NaturalNumber secondNumber){
     return *this;
 }
