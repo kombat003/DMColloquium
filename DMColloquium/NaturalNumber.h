f06f96ab33bb04cc3f75b94322389e5c2f69fed2
IlyaKulebyakin <kuler29@me.com>
2015-05-16 12:56:03 +0300

    reading from stream. Deleted declaration fix

--- DMColloquium/NaturalNumber.h
+++ DMColloquium/NaturalNumber.h
@@ -23,6 +23,7 @@ public:
     // ("23462346234523462346234623")xw
     NaturalNumber ADD_NN_N(NaturalNumber nat_num_2);
     friend std::ostream& operator<< (std::ostream& str, const NaturalNumber& number);
+    friend std::istream& operator>> (std::istream& str, NaturalNumber& number);
     // put declarations-ONLY for each method below
 
     int COM_NN_D(NaturalNumber);
